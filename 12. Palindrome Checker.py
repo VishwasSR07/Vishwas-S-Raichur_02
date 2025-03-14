7623b3271349542a6b3edbb3be408483f7f42845
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-14 23:50:49 +0530

    Create 12. Palindrome Checker.py

---
+++ 12. Palindrome Checker.py
@@ -0,0 +1,5 @@
+# # 12. Palindrome Checker
+s = input("Enter a number or string: ")
+print("Palindrome" if s == s[::-1] else "Not a palindrome")
+s = input("Enter a number or string: ")
+print("Palindrome" if s == s[::-1] else "Not a palindrome")
